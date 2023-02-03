9450564a229c1b788210109ee12f6647db56093e
Matthew Aniskevich <>
2023-02-03 15:37:15 +0100

    3

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthPaths } from "./core/constants/paths.constants";
 import ExampleWithSwitch from "./pages/ExampleWithSwitch/ExampleWithSwith";
 import { SignInPage } from "./pages/SignIn";
-import { SignInStatePage } from "./pages/SignInState";
 import { store } from "./store";
 
 export const AppRoutes = () => {
